fdb3b59e609f22474944f4b61bc4b0b62fc8f3c9
Igor Novak <novakin@amazon.com>
2019-07-07 20:45:36 +0200

    Store chat data in the dynamodb

--- index.js
+++ index.js
@@ -1,39 +1,55 @@
 const https = require('https')
+const aws = require("aws-sdk")
+
+// aws.config.update({region: 'eu-west-1'})
+
+const db = new aws.DynamoDB.DocumentClient({
+  region: 'eu-west-1'
+})
 
 const apiToken = require('./token').token
 
 exports.handler = async (event) => {
-    console.log(event.body);
-    
-    // Send a message to members
-    const message = JSON.parse(event.body).message
-    const params = {
-        chat_id: message.chat.id
-    }
-    let apiHandler
-    if (message.text) {
-        apiHandler = 'sendMessage'
-        params.text = message.text.toUpperCase()
-    } else if (message.photo) {
-        apiHandler = 'sendPhoto'
-        params.caption = message.caption
-        params.photo = message.photo[0].file_id
-    }
-    try {
-        const response = await post(apiHandler, params)
-        console.log('Message sent to chatId', params.chatId, response)
-    } catch (e) {
-        console.error('Failed to send a message to chatId', params.chatId, e)
-    }
+  console.debug(event.body);
 
-    const response = {
-        statusCode: 200,
-        body: JSON.stringify('Hello from Lambda! 1123'),
-    };
-    return response;
+  // Send a message to members
+  // const chatId = 178053996
+  const message = JSON.parse(event.body).message
+  const params = {}
+  let apiHandler
+  if (message.text) {
+    apiHandler = 'sendMessage'
+    params.text = message.text.toUpperCase()
+  } else if (message.photo) {
+    apiHandler = 'sendPhoto'
+    params.caption = message.caption.toUpperCase()
+    params.photo = message.photo[0].file_id
+  }
+
+  const chats = await db.scan({ TableName: 'chats' }).promise()
+    .then(response => response.Items.map(_ => _.id))
+    .catch(err => {
+      console.error('Unable to read item. Error JSON:', JSON.stringify(err, null, 2))
+      throw err
+    })
+  
+  try {
+    const promises = chats
+      .filter(id => id != message.chat.id)
+      .map(id => post(apiHandler, params, id))
+    await Promise.all(promises)
+  } catch (e) {
+    console.error('Failed to send a message to chatId', params.chatId, e)
+  }
+
+  const response = {
+    statusCode: 200,
+    body: JSON.stringify('Hello from Lambda! 1123'),
+  };
+  return response;
 };
 
-function post(handler, params) {
+function post(handler, params, chatId) {
   const options = {
     hostname: 'api.telegram.org',
     path: `/bot${ apiToken }/${ handler }`,
@@ -50,6 +66,7 @@ function post(handler, params) {
       })
       res.on('end', d => {
         if (res.statusCode == 200) {
+          console.debug('Message sent to chatId', chatId, response)
           resolve(JSON.parse(response))
         } else {
           reject(JSON.parse(response))
@@ -58,10 +75,10 @@ function post(handler, params) {
     })
 
     req.on('error', e => {
-      console.log(e);
+      console.error(e);
     })
 
-    req.write(JSON.stringify(params))
+    req.write(JSON.stringify({ ...params, chat_id: chatId }))
     req.end()
   });
 }
